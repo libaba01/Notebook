7feb41a357b787f51720a8508cb7263538e2726d
Libaba <Libaba@qq.com>
2022-09-01 20:50:12 +0800

    notebook

---
+++ Notebook/project/project-java/src/test/java/com/itheima/DemoTest.java
@@ -0,0 +1,16 @@
+package com.itheima;
+import org.junit.Test;
+import org.junit.Assert;
+
+
+
+public class DemoTest{
+     @Test
+     public void testSay(){
+
+        Demo d=new Demo();
+        String ret=d.say("maven");
+        Assert.assertEquals("hellomaven",ret);
+
+    }
+}
